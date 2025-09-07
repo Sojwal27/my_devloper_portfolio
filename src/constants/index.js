d31f6c2af395a48f523130fce14a66471f159a0e
Sojwal Sonkusale <133991474+Sojwal27@users.noreply.github.com>
2025-09-07 18:47:57 +0000

    update

---
+++ src/constants/index.js
@@ -0,0 +1,152 @@
+export const myProjects = [
+  {
+    id: 1,
+    title: "AURIX-personal_desktop_AI_assistant",
+    description:
+      "Aurix AI Desktop Assistant is an offline, voice-enabled AI assistant that works without internet. It uses Ollama for local AI processing, understands voice commands, speaks responses, launches apps, controls system settings, and provides a PyQt5-based desktop interface — all running entirely on your device for speed and privacy.",
+    subDescription: [
+    ],
+    href: "https://github.com/Sojwal27/AURIX-personal_desktop_AI_assistant",
+    logo: "",
+    image: "/assets/projects/System_intraction (1).png",
+    tags: [
+      {
+        id: 1,
+        name: "Python",
+        path: "/assets/logos/python.png",
+      },
+      {
+        id: 2,
+        name: "PyQt5",
+        path: "/assets/logos/PyQt.png",
+      },
+      {
+        id: 3,
+        name: "ollama",
+        path: "/assets/logos/ollama.png",
+      },
+      {
+        id: 4,
+        name: "PyAudio",
+        // path: "/assets/logos/tailwindcss.svg",
+      },
+       {
+        id: 5,
+        name: "ElevenLabs",
+        // path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Business Dashboard Project",
+    description:
+      "An interactive, data-driven business dashboard built during my internship, designed to handle 1.8+ million entries with high performance, scalability, and an engaging UI. This project demonstrates big data processing, backend integration, and full-stack development skills.",
+    subDescription: [
+    ],
+    href: "https://github.com/Sojwal27/Business_Dashboard_project",
+    logo: "",
+    image: "/assets/projects/bussiness_dashboard.png",
+    tags: [
+      {
+        id: 1,
+        name: "HTML5",
+        path: "/assets/logos/html5.svg",
+      },
+      {
+        id: 2,
+        name: "Python",
+        path: "/assets/logos/python.png",
+      },
+      {
+        id: 3,
+        name: "SQL",
+        path: "/assets/logos/sql.png",
+      },
+      {
+        id: 4,
+        name: "Git",
+        path: "/assets/logos/git.svg",
+      },
+       {
+        id: 5,
+        name: "Github",
+        path: "/assets/logos/github.svg",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Phishing-Detection-using-machine-learning",
+    description:
+      "Phishing detection leverages machine learning algorithms to identify and combat phishing attacks effectively. By analyzing features from URLs, email structures, and user behavior, models can adapt to evolving phishing tactics.",
+    subDescription: [
+    ],
+    href: "https://github.com/Sojwal27/Phishing-Detection-using-machine-learning",
+    logo: "",
+    image: "/assets/projects/Phishing_detection.png",
+    tags: [
+      {
+        id: 1,
+        name: "Python",
+        path: "/assets/logos/python.png",
+      },
+      {
+        id: 2,
+        name: "Machine Learning",
+        path: "/assets/logos/machine_learning.png",
+      },
+      {
+        id: 3,
+        name: "Google Colab",
+        path: "/assets/logos/google_colab.png",
+      },
+    ],
+  },
+];
+
+export const mySocials = [
+  {
+    name: "WhatsApp",
+    href: "https://wa.me/qr/JAQHFLI2EPNEB1",
+    icon: "/assets/socials/whatsApp.svg",
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/sojwal-sonkusale-7b35bb230/",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/sojwal__22/",
+    icon: "/assets/socials/instagram.svg",
+  },
+];
+
+export const experiences = [
+  {
+    title: "Ui Ux Designer and Video Editor",
+    job: "Aluet / Buzzhive",
+    date: "May 2024- Sep 2024",
+    contents: [
+        "Designed user-friendly and visually appealing UI layouts for web and mobile applications, improving overall user engagement.",
+        "Conducted user research, created wireframes, and developed interactive prototypes using tools like Figma and Adobe XD.",
+        "Edited promotional and explainer videos using Adobe Premiere Pro and After Effects, enhancing brand communication.",
+        "Collaborated with developers to ensure seamless integration of design assets into final products.",
+        "Optimized design workflows by implementing design systems and reusable components for faster turnaround.",
+    ]
+  },
+  {
+    title: "Software Engineer Intern",
+    job: "Stellanova Globaltech Pvt Ltd",
+    date: "Feb 2025- April 2025",
+    contents: [
+        "Cleaned and processed a large-scale dataset with over 1.8 million entries using Python (Pandas, NumPy) and SQL, ensuring data consistency and accuracy.",
+        "Handled missing values, removed duplicates, and standardized formats to prepare the data for backend integration.",
+        "Designed and developed a web-based business dashboard to display key metrics and insights using HTML, CSS, JavaScript, and data visualization libraries.",
+        "Implemented backend integration using Flask for efficient, scalable, and optimized data retrieval.",
+        "Gained hands-on experience in big data handling, data engineering, and full-stack development with a focus on user experience and performance.",
+    ]
+  },
+];
+
---
+++ src/sections/About.jsx
@@ -0,0 +1,137 @@
+import { useRef } from "react";
+import Card from "../components/Card";
+import { Globe } from "../components/globe";
+import CopyEmailButton from "../components/CopyEmailButton";
+import { Frameworks } from "../components/Frameworks";
+
+const About = () => {
+  const grid2Container = useRef();
+  return (
+    <section className="c-space section-spacing" id="about">
+      <h2 className="text-heading">About Me</h2>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-6 md:auto-rows-[18rem] mt-12">
+        {/* Grid 1 */}
+        <div className="flex items-end grid-default-color grid-1">
+          <img
+            src="assets/coding-pov.png"
+            className="absolute scale-[1.75] -right-[5rem] -top-[1rem] md:scale-[3] md:left-50 md:inset-y-10 lg:scale-[2.5]"
+          />
+          <div className="z-10">
+            <p className="headtext">Hi, I'm Sojwal Sonkusale</p>
+            <p className="subtext">
+              I’m a quick learner with a creative mindset, passionate about exploring every aspect of technology. I'm actively building my skills in AI, machine learning, web development, mobile app development, and databases to create innovative and impactful digital solutions.
+            </p>
+          </div>
+          <div className="absolute inset-x-0 pointer-evets-none -bottom-4 h-1/2 sm:h-1/3 bg-gradient-to-t from-indigo" />
+        </div>
+        {/* Grid 2 */}
+        <div className="grid-default-color grid-2">
+          <div
+            ref={grid2Container}
+            className="flex items-center justify-center w-full h-full"
+          >
+            <p className="flex items-end text-5xl text-gray-500">
+              CODE IS CRAFT
+            </p>
+            <Card
+              style={{ rotate: "75deg", top: "30%", left: "20%" }}
+              text="OOPs"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "-30deg", top: "60%", left: "45%" }}
+              text="SOLID"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "90deg", bottom: "30%", left: "70%" }}
+              text="YAGNI"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "10deg", bottom: "30%", left: "70%" }}
+              text="DRY"
+              containerRef={grid2Container}
+            />            
+            <Card
+              style={{ rotate: "-45deg", top: "55%", left: "0%" }}
+              text="Design Principles"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "20deg", top: "10%", left: "38%" }}
+              text="SRP"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "30deg", top: "70%", left: "70%" }}
+              image="assets/logos/c_img.png"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "-45deg", top: "70%", left: "25%" }}
+              image="assets/logos/python.png"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "-45deg", top: "5%", left: "10%" }}
+              image="assets/logos/codeImg.png"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "-45deg", top: "15%", left: "15%" }}
+              image="assets/logos/git.svg"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "9deg", top: "25%", left: "60%" }}
+              image="assets/logos/github.svg"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "9deg", top: "25%", left: "30%" }}
+              image="assets/logos/sql.png"
+              containerRef={grid2Container}
+            />
+          </div>
+        </div>
+        {/* Grid 3 */}
+        <div className="grid-black-color grid-3">
+          <div className="z-10 w-[50%]">
+            <p className="headtext">Time Zone</p>
+            <p className="subtext">
+              I'm based in Mars, and open to remote work worldwide
+            </p>
+          </div>
+          <figure className="absolute left-[30%] top-[10%]">
+            <Globe />
+          </figure>
+        </div>
+        {/* Grid 4 */}
+        <div className="grid-special-color grid-4">
+          <div className="flex flex-col items-center justify-center gap-4 size-full">
+            <p className="text-center headtext">
+              Do you want to start a project together?
+            </p>
+            <CopyEmailButton />
+          </div>
+        </div>
+        {/* Grid 5 */}
+        <div className="grid-default-color grid-5">
+          <div className="z-10 w-[50%]">
+            <p className="headText">Teck Stack</p>
+            <p className="subtext">
+              I specialize in a variety of languages, frameworks, and tools taht
+              allow me to build robust and scalable applications
+            </p>
+          </div>
+          <div className="absolute inset-y-0 md:inset-y-9 w-full h-full start-[50%] md:scale-125">
+            <Frameworks />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
